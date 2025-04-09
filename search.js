ce75d76c28a59c4b186124ed0cf565730a1d030d
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-04-09 19:53:58 +0300

    init commit

--- search.js
+++ search.js
@@ -12,3 +12,19 @@ document.querySelector('.search-button').addEventListener('click', function(e) {
         document.querySelectorAll('.searchable').forEach(el => el.style.display = 'block');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchButton = document.getElementById('search-button');
+    const searchWrapper = document.querySelector('.search-wrapper');
+    const whatsappButton = document.getElementById('whatsapp-button');
+
+    searchButton.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      // Toggle active state
+      searchWrapper.classList.toggle('active');
+      searchButton.classList.toggle('moved');
+      whatsappButton.classList.toggle('hidden');
+    });
+  });
+
